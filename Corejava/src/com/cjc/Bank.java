b1fa5cd74fc53ea349e26945343eb137af0c037b
jyoti <jyoti@192.168.1.60>
2024-12-27 11:01:50 +0530

    this is core java folder

---
+++ Corejava/src/com/cjc/Bank.java
@@ -0,0 +1,17 @@
+package com.cjc;
+
+public class Bank {
+
+	public void m1()
+	{
+		String name="SBI";
+		long Accountno=33370355173l;
+	}
+	public static void main(String args[])
+	{
+	Bank b=new Bank();	
+	b.m1();
+	}
+	}
+	
+
