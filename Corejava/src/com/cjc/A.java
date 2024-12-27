323f27c24597a85ad3097b736cbcfc7c97774dc1
jyoti <jyoti@192.168.1.60>
2024-12-27 11:36:38 +0530

    setter getter program

---
+++ Corejava/src/com/cjc/A.java
@@ -0,0 +1,20 @@
+package com.cjc;
+
+public class A {
+ int rollno;
+ String name;
+public int getRollno() {
+	return rollno;
+}
+public void setRollno(int rollno) {
+	this.rollno = rollno;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+ 
+ 
+}
---
+++ Corejava/src/com/cjc/Test.java
@@ -0,0 +1,13 @@
+package com.cjc;
+
+public class Test {
+ 
+	public static void main(String[] args) {
+		A a = new A();
+		a.setRollno(1);
+		a.setName("abc");
+		
+		System.out.println(a.getRollno());
+		System.out.println(a.getName());
+	}
+}
